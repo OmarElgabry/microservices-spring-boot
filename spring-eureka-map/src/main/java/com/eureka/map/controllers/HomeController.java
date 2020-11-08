305f4eeed6d9f10f8533f029d6c28c678519009f
Chrs Boyd <chrisboyd360@gmail.com>
2020-11-07 20:08:32 -0800

    Update HomeController.java
    can read and update, next stage switch to in memory database for ease of implementation

--- spring-eureka-map/src/main/java/com/eureka/map/controllers/HomeController.java
+++ spring-eureka-map/src/main/java/com/eureka/map/controllers/HomeController.java
@@ -1,7 +1,7 @@
 package com.eureka.map.controllers;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -19,7 +19,7 @@ public class HomeController {
 	private static final Logger log = LoggerFactory.getLogger(HomeController.class);
 
 	
-	private List<Map> maps;
+	private ArrayList<Map> maps;
 	@Autowired
 	private RestTemplate restTemplate;
 	
@@ -27,10 +27,10 @@ public class HomeController {
 	private Environment env;
 	
 	HomeController(){
-		maps = Arrays.asList(
+		maps = new ArrayList<Map>(Arrays.asList(
 				new Map(1, 49.335096, -122.937257, 100),
 				new Map(2, 49.322201, -122.980560, 200),
-				new Map(3, 49.323323, -122.971282, 300));
+				new Map(3, 49.323323, -122.971282, 300)));
 	}
 		
 	@RequestMapping("/")
@@ -42,7 +42,7 @@ public class HomeController {
 	}
 	
 	@RequestMapping("/listmaps")
-	public List<Map> getMaps() {
+	public ArrayList<Map> getMaps() {
 		
 		return maps;
 	}
@@ -50,7 +50,7 @@ public class HomeController {
 	@PostMapping("/addmap")
 	public Map addMap(int id, double latitude, double longitude, int range) {
 		Map m = new Map(id,latitude,longitude,range);
-//		maps.add(m);
+		maps.add(m);
 		return m;
 	}
 	
